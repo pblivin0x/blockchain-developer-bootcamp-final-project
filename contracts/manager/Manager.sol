46c792a95eac542e56cc16be500d805d11b29cce
pblivin0x <pblivin0x@gmail.com>
2021-11-14 11:06:31 -0500

    add streaming fee module

--- contracts/manager/Manager.sol
+++ contracts/manager/Manager.sol
@@ -1,6 +1,5 @@
 pragma solidity 0.6.10;
 
 import { Address } from "@openzeppelin/contracts/utils/Address.sol";
-
-import { AddressArrayUtils } from "../lib/AddressArrayUtils.sol";
 import { ISetToken } from "../interfaces/ISetToken.sol";
+import { IStreamingFeeModule } from "../interfaces/IStreamingFeeModule.sol";

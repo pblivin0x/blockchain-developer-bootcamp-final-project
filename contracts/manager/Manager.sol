90d6c83aaf1d97c5df89fdb40ba4ff063f573a80
pblivin0x <pblivin0x@gmail.com>
2021-11-13 21:57:52 -0500

    Add AddressArrayUtils.sol

--- contracts/manager/Manager.sol
+++ contracts/manager/Manager.sol
@@ -2,4 +2,5 @@ pragma solidity 0.6.10;
 
 import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 
+import { AddressArrayUtils } from "../lib/AddressArrayUtils.sol";
 import { ISetToken } from "../interfaces/ISetToken.sol";

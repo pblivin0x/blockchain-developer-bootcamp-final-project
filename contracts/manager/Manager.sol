da968fbfc8f80e2f6ab66c81085e1f0f0bc36366
pblivin0x <pblivin0x@gmail.com>
2021-11-14 17:37:23 -0500

    add external functions and events

--- contracts/manager/Manager.sol
+++ contracts/manager/Manager.sol
@@ -15,6 +15,22 @@ contract Manager {
 
     /* ============ Events ============ */
 
+    event FeesAccrued(
+        uint256 _totalFees,
+        uint256 _operatorTake,
+        uint256 _methodologistTake
+    );
+
+    event MethodologistChanged(
+        address _oldMethodologist,
+        address _newMethodologist
+    );
+
+    event OperatorChanged(
+        address _oldOperator,
+        address _newOperator
+    );
+
     /* ============ Modifiers ============ */
 
     /**
@@ -44,10 +60,10 @@ contract Manager {
     // Address of StreamingFeeModule
     IStreamingFeeModule public feeModule;
 
-    // Address of operator
+    // Address of operator which typically executes manager only functions on Set Protocol modules
     address public operator;
 
-    // Address of methodologist
+    // Address of methodologist which serves as providing methodology for the index
     address public methodologist;
 
     // Percent in 1e18 of streamingFees sent to operator
@@ -79,4 +95,155 @@ contract Manager {
     }
 
     /* ============ External Functions ============ */
+
+    /**
+     * OPERATOR ONLY: 
+     * Executes a trade on a supported DEX. Only callable by the SetToken's manager.
+     * @dev Although the SetToken units are passed in for the send and receive quantities, the total quantity
+     * sent and received is the quantity of SetToken units multiplied by the SetToken totalSupply.
+     *
+     * @param _setToken             Instance of the SetToken to trade
+     * @param _exchangeName         Human readable name of the exchange in the integrations registry
+     * @param _sendToken            Address of the token to be sent to the exchange
+     * @param _sendQuantity         Units of token in SetToken sent to the exchange
+     * @param _receiveToken         Address of the token that will be received from the exchange
+     * @param _minReceiveQuantity   Min units of token in SetToken to be received from the exchange
+     * @param _data                 Arbitrary bytes to be used to construct trade call data
+     */
+    function trade(
+        ISetToken _setToken,
+        string memory _exchangeName,
+        address _sendToken,
+        uint256 _sendQuantity,
+        address _receiveToken,
+        uint256 _minReceiveQuantity,
+        bytes memory _data
+    ) external onlyOperator {
+        tradeModule.trade(_setToken, _exchangeName, _sendToken, _sendQuantity, _receiveToken, _minReceiveQuantity, _data);
+    }
+
+    /**
+     * Accrue fees from streaming fee module and transfer tokens to operator / methodologist addresses based on fee split
+     */
+    function accrueFeeAndDistribute() public {
+        feeModule.accrueFee(setToken);
+
+        uint256 setTokenBalance = setToken.balanceOf(address(this));
+
+        uint256 operatorTake = setTokenBalance.preciseMul(operatorFeeSplit);
+        uint256 methodologistTake = setTokenBalance.sub(operatorTake);
+
+        setToken.transfer(operator, operatorTake);
+
+        setToken.transfer(methodologist, methodologistTake);
+
+        emit FeesAccrued(setTokenBalance, operatorTake, methodologistTake);
+    }
+
+    /**
+     * OPERATOR ONLY: Update the SetToken manager address. 
+     *
+     * @param _newManager           New manager address
+     */
+    function updateManager(address _newManager) external onlyOperator {
+        require(_newManager != address(0), "Zero address not valid");
+        setToken.setManager(_newManager);
+    }
+
+    /**
+     * OPERATOR ONLY: Add a new module to the SetToken.
+     *
+     * @param _module           New module to add
+     */
+    function addModule(address _module) external onlyOperator {
+        setToken.addModule(_module);
+    }
+
+    /**
+     * OPERATOR ONLY: Interact with a module registered on the SetToken. Cannot be used to call functions in the
+     * fee module, due to ability to bypass methodologist permissions to update streaming fee.
+     *
+     * @param _module           Module to interact with
+     * @param _data             Byte data of function to call in module
+     */
+    function interactModule(address _module, bytes calldata _data) external onlyOperator {
+        require(_module != address(feeModule), "Must not be fee module");
+
+        // Invoke call to module, assume value will always be 0
+        _module.functionCallWithValue(_data, 0);
+    }
+
+    /**
+     * OPERATOR ONLY: Remove a new module from the SetToken.
+     *
+     * @param _module           Module to remove
+     */
+    function removeModule(address _module) external onlyOperator {
+        setToken.removeModule(_module);
+    }
+
+    /**
+     * METHODOLOGIST ONLY: Update the streaming fee for the SetToken.
+     *
+     * @param _newFee           New streaming fee percentage
+     */
+    function updateStreamingFee(uint256 _newFee) external onlyMethodologist {
+        feeModule.updateStreamingFee(setToken, _newFee);
+    }
+
+    /**
+     * OPERATOR ONLY: Update the fee recipient address.
+     *
+     * @param _newFeeRecipient           New fee recipient address
+     */
+    function updateFeeRecipient(address _newFeeRecipient) external onlyOperator {
+        feeModule.updateFeeRecipient(setToken, _newFeeRecipient);
+    }
+
+    /**
+     * OPERATOR ONLY: Update the fee split percentage.
+     *
+     * @param _newFeeSplit           New fee split percentage
+     */
+    function updateFeeSplit(uint256 _newFeeSplit) external onlyOperator {
+        require(
+            _newFeeSplit <= PreciseUnitMath.preciseUnit(),
+            "Operator Fee Split must be less than 1e18"
+        );
+
+        // Accrue fee to operator and methodologist prior to new fee split
+        accrueFeeAndDistribute();
+        operatorFeeSplit = _newFeeSplit;
+    }
+
+    /**
+     * OPERATOR ONLY: Update the trade module
+     *
+     * @param _newTradeModule           New trade module
+     */
+    function updateTradeModule(ITradeModule _newTradeModule) external onlyOperator {
+        tradeModule = _newTradeModule;
+    }
+
+    /**
+     * METHODOLOGIST ONLY: Update the methodologist address
+     *
+     * @param _newMethodologist           New methodologist address
+     */
+    function updateMethodologist(address _newMethodologist) external onlyMethodologist {
+        emit MethodologistChanged(methodologist, _newMethodologist);
+
+        methodologist = _newMethodologist;
+    }
+
+    /**
+     * OPERATOR ONLY: Update the operator address
+     *
+     * @param _newOperator           New operator address
+     */
+    function updateOperator(address _newOperator) external onlyOperator {
+        emit OperatorChanged(operator, _newOperator);
+
+        operator = _newOperator;
+    }
 }

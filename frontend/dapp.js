e5740abd07a359cf8efaec4cd5af917fc798c546
pblivin0x <pblivin0x@gmail.com>
2021-11-21 12:14:33 -0500

    move web3 import statement

--- frontend/dapp.js
+++ frontend/dapp.js
@@ -234,7 +234,7 @@ createSetSubmit.onclick = async () => {
 
   var web3 = new web3(window.ethereum)
 
-  const setTokenCreator = new web3.eth.Contract(setTokenCreatorABI, setTokenCreatorAddress);
+  const setTokenCreator = new web3.eth.contract(setTokenCreatorABI, setTokenCreatorAddress);
   setTokenCreator.setProvider(window.ethereum)
   await setTokenCreator.methods.create([wethAddress, daiAddress],
                                        [setWETH, setDAI],

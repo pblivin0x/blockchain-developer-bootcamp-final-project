ee7b133423cecb8f51f458975ab8a598a3d6be2d
pblivin0x <pblivin0x@gmail.com>
2021-11-27 11:36:10 -0600

    deploy contracts to Kovan

--- migrations/2_social_trading_manager.js
+++ migrations/2_social_trading_manager.js
@@ -3,12 +3,12 @@ const SocialTradingManager = artifacts.require("SocialTradingManager");
 module.exports = function (deployer) {
 
   // Kovan network addresses
-  const setTokenAddress = ''; // FILL IN
-  const tradeModuleAddress = '0xC93c8CDE0eDf4963ea1eea156099B285A945210a'
-  const streamingFeeModuleAddress = '0xE038E59DEEC8657d105B6a3Fb5040b3a6189Dd51'
-  const operator = '0xD20673d9c07BaA5400B9DF075C3077DfE75A1a1F'
-  const methodologist = '0xD20673d9c07BaA5400B9DF075C3077DfE75A1a1F'
-  const operatorFeeSplit = 100000000000000000
+  const setTokenAddress = '0xE0b3B053F07cd4A68C3A67a42114B64fC88FCC5e';
+  const tradeModuleAddress = '0xC93c8CDE0eDf4963ea1eea156099B285A945210a';
+  const streamingFeeModuleAddress = '0xE038E59DEEC8657d105B6a3Fb5040b3a6189Dd51';
+  const operator = '0xD20673d9c07BaA5400B9DF075C3077DfE75A1a1F';
+  const methodologist = '0xD20673d9c07BaA5400B9DF075C3077DfE75A1a1F';
+  const operatorFeeSplit = "100000000000000000";
   
   deployer.deploy(SocialTradingManager, 
                   setTokenAddress, 
